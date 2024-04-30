047b918e52f4888064a3898e30778f49e5d2b389
zhangqiuyang <zhangqiuyang@bybest.cn>
2024-04-30 15:03:36 +0800

    完善火山发信SDK

--- src/Base/SignatureV4.php
+++ src/Base/SignatureV4.php
@@ -93,7 +93,7 @@ class SignatureV4
         }
 
         try {
-            return Psr7\hash($request->getBody(), 'sha256');
+            return Psr7\Utils::hash($request->getBody(), 'sha256');
         } catch (\Exception $e) {
             throw new CouldNotCreateChecksumException('sha256', $e);
         }
@@ -232,7 +232,7 @@ class SignatureV4
         return [
             'method'  => $request->getMethod(),
             'path'    => $uri->getPath(),
-            'query'   => Psr7\parse_query($uri->getQuery()),
+            'query'   => Psr7\Query::parse($uri->getQuery()),
             'uri'     => $uri,
             'headers' => $request->getHeaders(),
             'body'    => $request->getBody(),
@@ -243,7 +243,7 @@ class SignatureV4
     private function buildRequest(array $req)
     {
         if ($req['query']) {
-            $req['uri'] = $req['uri']->withQuery(Psr7\build_query($req['query']));
+            $req['uri'] = $req['uri']->withQuery(Psr7\Query::build($req['query']));
         }
 
         return new Psr7\Request(
@@ -258,7 +258,7 @@ class SignatureV4
     private function buildRequestString(array $req)
     {
         if ($req['query']) {
-            $req['uri'] = $req['uri']->withQuery(Psr7\build_query($req['query']));
+            $req['uri'] = $req['uri']->withQuery(Psr7\Query::build($req['query']));
         }
         return (string)$req['uri'];
     }
